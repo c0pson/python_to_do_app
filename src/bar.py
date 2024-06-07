19458441c77ab2afa4d839154070646321a09a25
Piotr Copek <pc312199@student.polsl.pl>
2024-06-07 13:13:58 +0200

    added edit dictionary function

--- src/bar.py
+++ src/bar.py
@@ -69,3 +69,10 @@ class EventsBar(ctk.CTkFrame):
         self.button_dict[key].destroy()
         del self.button_dict[key]
         del self.events[key]
+
+    def edit_in_dictionary(self, key: int, new_data: str) -> None:
+        title = f'{self.events[key].split(',',1)[0]}'
+        self.button_dict[key].configure(text=title)
+        self.current_content.destroy()
+        self.load_event_content(key)
+        self.events[key] = new_data
