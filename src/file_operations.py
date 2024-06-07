f2cdde520d8d82709c7c67b8b00d6094607f560d
Piotr Copek <pc312199@student.polsl.pl>
2024-06-07 13:15:01 +0200

    edited edit option

--- src/file_operations.py
+++ src/file_operations.py
@@ -15,18 +15,13 @@ def remove_event(events: dict, key: int) -> None:
     with open(file_name, 'w') as file:
         file.write(new_content)
 
-def edit_event(events: dict, key: int, event_edit: str, edit_region: str) -> None:
+def edit_event(events: dict, key: int, event_edit: str) -> None:
     """Function editing database with all user events.
 
     Args:
         events (dict): dictionary of all events
         key (int): key of the event in dictionary
         event_edit (str): string with which info will be replaced
-        edit_region (str): 
-            - 0 : name
-            - 1 : date
-            - 2 : time
-            - 3 : description
     """
     file_name: str = f'{os.path.join(os.path.dirname(__file__), '..') + Paths.EVENTS}'
     new_content: str = ''
@@ -35,8 +30,6 @@ def edit_event(events: dict, key: int, event_edit: str, edit_region: str) -> Non
             if event != key:
                 new_content += f'{events[event]}\n'
             else:
-                event_to_edit = events[event].split(',')
-                event_to_edit[edit_region] = event_edit
-                new_content += f'{''.join(event_to_edit)}\n'
+                new_content += f'{event_edit}\n'
     with open(file_name, 'w') as file:
         file.write(new_content)
--- src/new_event.py
+++ src/new_event.py
@@ -3,13 +3,30 @@ import customtkinter as ctk
 # Imported properties
 from properties import Colors
 
-class AddNewEvent(ctk.CTkLabel):
+class TopFrame(ctk.CTkFrame):
     def __init__(self, master):
-        super().__init__(master, 
-                        text='+', font=('Comic Sans', 41),
-                        fg_color=Colors.BACKGROUND, bg_color=Colors.MAIN_COLOR,
-                        height=50, width=50, corner_radius=10)
+        self.master = master
+        super().__init__(self.master, fg_color=Colors.ACCENT_COLOR_2,
+                        corner_radius=0)
+        self.place(relx=0.5, rely=0.5, anchor=ctk.CENTER,
+                    relwidth=1.01, relheight=1.01)
         self.setup()
 
     def setup(self):
         pass
+
+class AddNewEvent(ctk.CTkLabel):
+    def __init__(self, master):
+        self.master = master
+        super().__init__(self.master, 
+                        text='+', font=('Comic Sans', 41),
+                        fg_color=Colors.BACKGROUND, bg_color=Colors.MAIN_COLOR,
+                        height=50, width=50, corner_radius=10)
+        self.bind('<Button-1>', lambda event: self.ask_for_new_event(event))
+        self.setup()
+
+    def setup(self):
+        pass
+
+    def ask_for_new_event(self, event):
+        TopFrame(self.master)
